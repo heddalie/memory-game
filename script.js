03ee61c632cd484a73a79f7519ce19ae3a441bb5
Hedda Lie <hedda.paulsen.lie@gmail.com>
2023-06-02 13:51:25 +0200

    grid test using functions for responsiveness

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 console.log('JavaScript code loaded');
 // i'll keep this until i don't have problems
+
+
+// responsive grid
+window.addEventListener('DOMContentLoaded', () => { //makes sure DOM is loaded
+    const gridContainer = document.getElementById('gridContainer');
+    resizeSquares();
+
+    //resizeSquares will calculate the square size for the cards
+
+    window.addEventListener('resize', resizeSquares);
+
+    function resizeSquares() {
+        const containerWidth = gridContainer.offsetWidth;
+        const squareSize = containerWidth / 8 - 5; //the -1 is for the gapsize
+        const gridItems = Array.from(gridContainer.getElementsByClassName('grid-item'));
+
+        gridItems.forEach(item => {
+            item.style.width = squareSize + 'px';
+            item.style.height = squareSize + 'px';
+        });
+    }
+    });
