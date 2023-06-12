2d0692971272388bb1ebad3d50431b7968387c1d
Hedda Lie <hedda.paulsen.lie@gmail.com>
2023-06-12 11:55:34 +0200

    responsive!! added breakpoint

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('JavaScript code loaded');
+
+const gridContainer = document.getElementById('grid-container');
+
+const rows = 5;
+const columns = 8;
+
+gridContainer.style.gridTemplateColumns="repeat("+columns+",1fr)";
+
+for (let i = 0; i < rows * columns; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+
+    gridItem.innerHTML=i;
+    console.log(i)
+
+    gridItem.addEventListener('click', function(event) {
+        const clickedElement = event.target;
+    
+        console.log('Clicked on field:', clickedElement);
+    });
+}
