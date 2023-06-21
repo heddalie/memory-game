7c4c163ca574d048c9ea31a79266de1ee15012c2
Hedda Lie <hedda.paulsen.lie@gmail.com>
2023-06-21 18:15:06 +0200

    game works
    cards don't disappear on when alike

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('JavaScript code loaded');
 // keeping this until i don't have problems anymore
 
+
 //append generated cards
 const gridContainer = document.getElementById('grid-container');
 
@@ -24,33 +25,66 @@ restartButton.addEventListener('click', restartGame);
 
 //path to pngs/symbols
 const symbols = [
-  'symbols/Symbol-1.png',
-  'symbols/Symbol-2.png',
-  'symbols/Symbol-3.png',
-  'symbols/Symbol-4.png',
-  'symbols/Symbol-5.png',
-  'symbols/Symbol-6.png',
-  'symbols/Symbol-7.png',
-  'symbols/Symbol-8.png',
-  'symbols/Symbol-9.png',
-  'symbols/Symbol-10.png',
-  'symbols/Symbol-11.png',
-  'symbols/Symbol-12.png',
-  'symbols/Symbol-13.png',
-  'symbols/Symbol-14.png',
-  'symbols/Symbol-15.png',
-  'symbols/Symbol-16.png',
-  'symbols/Symbol-17.png',
-  'symbols/Symbol-18.png',
-  'symbols/Symbol-19.png',
-  'symbols/Symbol-20.png'
+  { name: "pic1", image: 'symbols/Symbol-1.png'},
+  { name: "pic2", image: 'symbols/Symbol-2.png'},
+  { name: "pic3", image: 'symbols/Symbol-3.png'},
+  { name: "pic4", image: 'symbols/Symbol-4.png'},
+  { name: "pic5", image: 'symbols/Symbol-5.png'},
+  { name: "pic6", image: 'symbols/Symbol-6.png'},
+  { name: "pic7", image: 'symbols/Symbol-7.png'},
+  { name: "pic8", image: 'symbols/Symbol-8.png'},
+  { name: "pic9", image: 'symbols/Symbol-9.png'},
+  { name: "pic10", image: 'symbols/Symbol-10.png'},
+  { name: "pic11", image: 'symbols/Symbol-11.png'},
+  { name: "pic12", image: 'symbols/Symbol-12.png'},
+  { name: "pic13", image: 'symbols/Symbol-13.png'},
+  { name: "pic14", image: 'symbols/Symbol-14.png'},
+  { name: "pic15", image: 'symbols/Symbol-15.png'},
+  { name: "pic16", image: 'symbols/Symbol-16.png'},
+  { name: "pic17", image: 'symbols/Symbol-17.png'},
+  { name: "pic18", image: 'symbols/Symbol-18.png'},
+  { name: "pic19", image: 'symbols/Symbol-19.png'},
+  { name: "pic20", image: 'symbols/Symbol-20.png'}
 ];
+
+// Pick random objects from the items array
+const generateRandom = () => {
+  // temporary array
+  let tempArray = [...symbols];
+  // initializes cardValues array
+  let cardValues = [];
+  // Random object selection
+  for (let i = 0; i < 40; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    // once selected, remove the object from the temporary array
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
+
+const matrixGenerator = (cardValues) => {
+  gridContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  // simple shuffle
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < 40; i++) {
+    /*
+        Create Cards
+        before => front side (contains question mark)
+        after => back side (contains actual image);
+        data-card-values is a custom attribute which stores the names of the cards to match later
+      */
+    const card = createCard(cardValues[i]);
+    gridContainer.appendChild(card);
+  }
+};
 
 //creates array & pairs
 const cards = symbols.concat(symbols);
 
 //shuffle cards array randomly
-shuffle(cards);
+//shuffle(cards);
 
 //creates card element for each symbol
 for (let i = 0; i < cards.length; i++) {
@@ -61,44 +95,47 @@ for (let i = 0; i < cards.length; i++) {
 //creates card element with front and backside
 //symbol applies on backside of card
 function createCard(symbol) {
+  console.log("create card");
   const card = document.createElement('div');
   card.classList.add('card');
 
-  //creates frontside of cards
   const frontSide = document.createElement('div');
-  frontSide.classList.add('card-front');
+  frontSide.classList.add('front-side');
   card.appendChild(frontSide);
 
-
-  //creates backside of cards
   const backSide = document.createElement('div');
-  backSide.classList.add('card-back');
-  backSide.style.backgroundImage = `url(${symbol})`;
+  backSide.classList.add('back-side');
   card.appendChild(backSide);
 
-  //click is added to the card element
-  //when a card is clicked, the function is executed
+  //triggers when a card is clicked
   card.addEventListener('click', function () {
-    if (!card.classList.contains('card-flipped') && !card.classList.contains('face-up') && openedCards.length < 2) {
+    if (!card.classList.contains('flipped') && !card.classList.contains('face-up') && openedCards.length < 2) {
+      console.log("click");
       if (!timerStarted) {
         timerStarted = true;
         timerInterval = setInterval(timeGenerator, 1000);
       }
-      card.classList.add('card-flipped');
+
+      card.classList.add('flipped');
+      const imgElement = card.querySelector('.back-side');
+      const cardName = card.getAttribute('data-name');
+      console.log(cardName);
+      imgElement.style.backgroundImage = `url(${cardName})`;
+
       openedCards.push(card);
       if (openedCards.length === 2) {
-        setTimeout(checkMatchingCards, 800);
+        setTimeout(checkMatchingCards, 500);
       }
-    } else if (card.classList.contains('card-flipped') && !card.classList.contains('face-up')) {
+    } else if (card.classList.contains('flipped') && !card.classList.contains('face-up')) {
       //do nothing if the card is already flipped and not face-up (matched)
       return;
     } else {
-      card.classList.remove('card-flipped');
+      card.classList.remove('flipped');
       openedCards.splice(openedCards.indexOf(card), 1);
     }
   });
 
-  card.setAttribute('data-symbol', symbol);
+  card.setAttribute('data-name', symbol.image);
 
   return card;
 }
@@ -108,30 +145,29 @@ function createCard(symbol) {
 function checkMatchingCards() {
   const card1 = openedCards[0];
   const card2 = openedCards[1];
-  const symbol1 = card1.querySelector('.card-back').style.backgroundImage; 
-  const symbol2 = card2.querySelector('.card-back').style.backgroundImage; 
+  const symbol1 = card1.getAttribute('data-name');
+  const symbol2 = card2.getAttribute('data-name');
 
   if (symbol1 === symbol2) {
-    //cards match
     score += 2;
     updateScore(score);
 
-    card1.style.opacity = '0';
-    card2.style.opacity = '0';
-
     openedCards = [];
 
-    //check for a win condition
-    const faceUpCards = document.querySelectorAll('.card.face-up');
-    if (faceUpCards.length === cards.length) {
-      //all cards have been matched, trigger the win condition
+    // Overlay after winning a game
+    if (score === cards.length) {
       clearInterval(timerInterval);
       showOverlayWithDelay();
     }
   } else {
-    setTimeout(function () {
-      card1.classList.remove('card-flipped');
-      card2.classList.remove('card-flipped');
+    setTimeout(() => {
+      card1.classList.remove('flipped');
+      card2.classList.remove('flipped');
+      const imgElement1 = card1.querySelector('.back-side');
+      const imgElement2 = card2.querySelector('.back-side');
+      imgElement1.style.backgroundImage = '';
+      imgElement2.style.backgroundImage = '';
+
       openedCards = [];
     }, 800);
   }
@@ -176,10 +212,10 @@ function restartGame() {
   score = 0;
   updateScore(score);
 
-  const gridItems = document.querySelectorAll('.card');
-  gridItems.forEach(function (item) {
-    item.remove();
-  });
+  // const gridItems = document.querySelectorAll('.card');
+  // gridItems.forEach(function (item) {
+  //   item.remove();
+  // });
 
   //clear timer interval
   clearInterval(timerInterval);
@@ -188,13 +224,9 @@ function restartGame() {
   timerStarted = false;
   timerValue.innerHTML = "<span>Time:</span> 00:00";
 
-  //shuffle
-  shuffle(cards);
-
-  for (let i = 0; i < cards.length; i++) {
-    const card = createCard(cards[i]);
-    gridContainer.appendChild(card);
-  }
+  //generate random card values and create the matrix
+  const cardValues = generateRandom();
+  matrixGenerator(cardValues);
 
   //hide overlay when restarting the game
   hideOverlay();
@@ -202,12 +234,12 @@ function restartGame() {
 
 
 //shuffles elements
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
 
 //overlay
 const overlayContainer = document.getElementById("overlay-container");
